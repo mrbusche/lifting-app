08e75b1778c2bb9eb26d7f465f6d2866ac7c4333
Matt Busche <mrbusche@gmail.com>
2026-09-26 10:48:59 -0500

    migrate to vite plus

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, lazyPlugins } from 'vite-plus';
 
 export default defineConfig({
-  plugins: [sveltekit(), tailwindcss()],
+  staged: {
+    '*': 'vp check --fix',
+  },
+  lint: {
+    jsPlugins: [{ name: 'vite-plus', specifier: 'vite-plus/oxlint-plugin' }],
+    rules: { 'vite-plus/prefer-vite-plus-imports': 'error' },
+    options: { typeAware: true, typeCheck: true },
+  },
+  fmt: {
+    ignorePatterns: ['node_modules'],
+    printWidth: 140,
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    sortImports: true,
+    sortTailwindcss: true,
+    svelte: true,
+    sortPackageJson: true,
+  },
+  plugins: lazyPlugins(() => [sveltekit(), tailwindcss()]),
 });
